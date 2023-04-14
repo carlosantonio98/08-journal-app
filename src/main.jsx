08bdea773c362802099f350a3da23cbbce0bc522
carloscamacho <carlosalvarez9805@gmail.com>
2023-04-13 18:35:12 -0600

    MUI setup with Vite

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 
 import { JournalApp } from './JournalApp';
 import './style.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <JournalApp />
-  </React.StrictMode>,
+    <React.StrictMode>
+        <BrowserRouter>
+            <JournalApp />
+        </BrowserRouter>
+    </React.StrictMode>
 )
