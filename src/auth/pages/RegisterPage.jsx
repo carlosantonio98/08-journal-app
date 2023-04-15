ff0b0ef17df5beb5fb1d69d45e3196991d3f197d
carloscamacho <carlosalvarez9805@gmail.com>
2023-04-14 19:13:59 -0600

    AuthLayout

--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -1,8 +1,11 @@
+import { AuthLayout } from "../layout/AuthLayout"
 
 export const RegisterPage = () => {
     return (
         <>
-            <h1>Register page</h1>
+            <AuthLayout title="Register">
+
+            </AuthLayout>
         </>
     )
 }
