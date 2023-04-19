aa1c1cdcd90fcc407f653c4f812a39b317d403b3
carloscamacho <carlosalvarez9805@gmail.com>
2023-04-19 17:21:59 -0600

    NothingSelectedView

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,13 +1,13 @@
-import { Typography } from '@mui/material';
 import { JournalLayout } from '../layout/JournalLayout';
+import { NothingSelectedView } from '../views';
 
 export const JournalPage = () => {
     return (
         <JournalLayout>
 
-            <Typography>Ad et dolor proident occaecat esse anim est consequat velit aliqua aliquip reprehenderit. Id excepteur et elit fugiat proident exercitation non enim reprehenderit deserunt tempor tempor. Veniam duis ullamco proident in do laboris ex adipisicing sint sit.</Typography>
+            {/* <Typography>Ad et dolor proident occaecat esse anim est consequat velit aliqua aliquip reprehenderit. Id excepteur et elit fugiat proident exercitation non enim reprehenderit deserunt tempor tempor. Veniam duis ullamco proident in do laboris ex adipisicing sint sit.</Typography> */}
 
-            {/* NothinSelected */}
+            <NothingSelectedView />
             
             {/* NoteView */}
 
