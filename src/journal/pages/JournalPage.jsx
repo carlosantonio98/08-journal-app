bf6b99eeabf43f57ebd5d5a935ff06f6c6c7fdcd
carloscamacho <carlosalvarez9805@gmail.com>
2023-04-20 17:40:31 -0600

    Floating button

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,3 +1,6 @@
+import { IconButton } from '@mui/material';
+import { AddOutlined } from '@mui/icons-material';
+
 import { JournalLayout } from '../layout/JournalLayout';
 import { NoteView, NothingSelectedView } from '../views';
 
@@ -10,6 +13,20 @@ export const JournalPage = () => {
             {/* <NothingSelectedView /> */}
             
             <NoteView />
+
+            <IconButton
+                size='large'
+                sx={{   // No usamos el style para asignar estilo, porque este no tiene acceso global a las propiedades del theme
+                    color: 'white',
+                    backgroundColor: 'error.main',
+                    ':hover': { backgroundColor: 'error.main', opacity: 0.9 },
+                    position: 'fixed',
+                    right: 50,
+                    bottom: 50,
+                }}
+            >
+                <AddOutlined sx={{ fontFamily: 30 }} />
+            </IconButton>
 
         </JournalLayout>
     )
