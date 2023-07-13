f22dff40f6e04dda4d504e0da260e960a056a0c9
carloscamacho <carlosalvarez9805@gmail.com>
2023-07-13 17:28:14 -0600

    feat: validate from our custom hook

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,8 +1,14 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export const useForm = ( initialForm = {}, formValidations = {} ) => {
   
     const [ formState, setFormState ] = useState( initialForm );
+    const [ formValidation, setFormValidation ] = useState({});  // La idea de este formValidation es que me diga a mi claramente si hay un error o no hay un error, usamos un useState porque necesitamos redibujar el formulario si llega a ocurrir algun error, siempre que queramos redibujar una parte del html el componente debe de actualizar el estado para que este en automatico redibuje el componente completo. Cuando estamos hablande de realizar cambios en el html que requieran renderizarse nuevamente tenemos que usar un tipo de hook que maneje el estado.
+
+    useEffect(() => {
+      createValidators();
+    }, [formState]) // Cada que cambie el formState se ejecutara el createValidators().
+    
 
     const onInputChange = ({ target }) => {
         const { name, value } = target;
@@ -16,10 +22,25 @@ export const useForm = ( initialForm = {}, formValidations = {} ) => {
         setFormState( initialForm );
     }
 
+    const createValidators = () => {
+
+        const formCheckedValues = {};
+
+        for (const formField of Object.keys( formValidations )) {
+            const [ fn, errorMessage ] = formValidations[ formField ];
+
+            formCheckedValues[`${ formField }Valid`] = fn( formState[formField] ) ? null : errorMessage;
+        }
+
+        setFormValidation( formCheckedValues );
+    }
+
     return {
         ...formState,
         formState,
         onInputChange,
         onResetForm,
+
+        ...formValidation
     }
 }
