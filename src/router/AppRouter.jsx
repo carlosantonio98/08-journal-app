a255a88f2ab8fd778feca751445deafa70e46cd0
carloscamacho <carlosalvarez9805@gmail.com>
2023-07-20 16:49:40 -0600

    feat: checking authentication

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,8 +1,17 @@
 import { Route, Routes } from "react-router-dom"
 import { AuthRoutes } from "../auth/routes/AuthRoutes"
 import { JournalRoutes } from "../journal/routes/JournalRoutes"
+import { useSelector } from "react-redux"
+import { CheckingAuth } from "../ui"
 
 export const AppRouter = () => {
+
+  const { status } = useSelector( state => state.auth );
+  
+  if ( status === 'checking' ) {
+    return <CheckingAuth />
+  }
+
   return (
     <Routes>  {/* Otra manera de gestionar las rutas */}
 
